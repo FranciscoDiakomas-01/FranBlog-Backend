b5860259369bb94771b9e478f2f040c9b857cf68
Francisco <franciscodiakoma@gmail.com>
2024-10-27 05:47:39 +0100

    inciando

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -57,12 +57,54 @@ export default class CategoryPost {
     return new Promise((resolve, reject) => {
       //total Post , Comment, APt E N
       if (typeof categoryId == "number" && categoryId > 0) {
+
+        //pegando os dados da categoria
         this.querySql = "select * from categoryPost where id = ?;";
         db.query(this.querySql, [categoryId], (err, result) => {
           if (err) {
-            reject(err.message);
+            reject(err);
           } else {
-            resolve(result.length > 0 ? result : "not found");
+            
+            const categoryData = result.length > 0 ? result : "not found";
+            if (categoryData === 'not found') {
+              reject('not found')
+            } else {
+              
+              //pegando o número de post dessa categoria
+              this.querySql = "select count(*) as totalPost from post where categoryId = ?;";
+              db.query(this.querySql, [categoryId], (err, result) => {
+                if (err) {
+                  reject(err);
+                } else {
+                  
+                  const totalCategory = result.length > 0 ? result[0].total : "not found";
+                  if (totalCategory === "not found") {
+                    reject('not found post')
+                  } else {
+                    //pegando o total de post / categoria / estado / apto ou não apto
+                    this.querySql ="select count(*) as total from post where categoryId = ? group by status ;";
+                    db.query(this.querySql, [categoryId], (err, result) => {
+                      if (err) {
+                        reject(err);
+                      } else {
+                        const postPerCategory = result.length > 0 ? result[0] : "not found";
+                        if (postPerCategory == 'not found') {
+                          reject('not found')
+                        } else {
+                          const result = {
+                            totalCategory : totalCategory,
+                            postPerCategory : postPerCategory,
+                            categoryData : categoryData,
+                          };
+                          resolve(result)
+                        }
+                      }
+                    });
+                  }
+                    
+                }
+              });
+            }
           }
         });
       } else {
@@ -75,7 +117,7 @@ export default class CategoryPost {
       this.querySql = "select * from categoryPost;";
       db.query(this.querySql, (err, result) => {
         if (err) {
-          reject(err.message);
+          reject(err);
         } else {
           resolve(result.length > 0 ? result : "not found");
         }
@@ -110,7 +152,7 @@ let data = {
   id : 1
 }
 
-category.getUserDetails(1).then(result => {
+category.createCatgoryPost(data).then(result => {
   console.log(result)
 }).catch(err => {
   console.log(err)
--- src/models/PostMdel.js
+++ src/models/PostMdel.js
@@ -1,88 +1,105 @@
 import validator from "validator";
 import db from "../db.js";
 db.connect();
-export default class User {
+
+export default class Post {
   #querySql = "";
   #values = [];
-  createUser(user) {
+  createPost(post) {
     return new Promise((resolve, reject) => {
-      if (
-        validator.isEmail(user.email) &&
-        validator.isStrongPassword(user.pass)
-      ) {
-        if (user.name.length <= 1 || user.profile.length <= 0 || !user.status) {
-          reject("missing name , profile or status");
-        } else {
-          this.querySql =
-            "insert into user(name, profile , status ,email , type) value(?);";
-          this.#values = [
-            user.name,
-            user.profile,
-            user.status,
-            user.email,
-            user.type,
-          ];
-          db.query(this.querySql, [this.#values], (err, result) => {
-            if (err) {
-              if (err.message.includes("Duplicate entry")) {
-                return reject("already exist");
-              }
-              reject(err.message);
-            } else {
-              resolve(result.insertId);
-            }
-          });
-        }
-      } else {
-        reject("invalid email or not strong password");
-      }
-    });
-  }
-  updateUser(user) {
-    return new Promise((resolve, reject) => {
-      if (
-        validator.isEmail(user.email) &&
-        validator.isStrongPassword(user.pass) &&
-        typeof user.id === "number" &&
-        user.id > 0
-      ) {
-        if (user.name.length <= 1 || user.profile.length <= 0 || !user.status) {
-          reject("missing name , profile or status");
-        } else {
-          this.querySql =
-            "update user set name = ?, profile = ? , status = ? , email = ? , type = ? where id = ?;";
-          db.query(
-            this.querySql,
-            [
-              user.name,
-              user.profile,
-              user.status,
-              user.email,
-              user.type,
-              user.id,
-            ],
-            (err, result) => {
-              if (err) {
-                if (err.message.includes("Duplicate entry")) {
-                  return reject("already exist");
+      if (!post.title.length <= 1 && typeof post.categoryId == "number") {
+        //verificando se existe uma categoria com esse id
+
+        this.#querySql = "select id from categoryPost where id = ?";
+        db.query(this.#querySql, [post.categoryId], (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            if (result[0]?.id) {
+              this.querySql =
+                "insert into post(title, description , categoryId ,cover , created_at , status) value(?);";
+              this.#values = [
+                post.title,
+                post.description,
+                post.categoryId,
+                post.cover,
+                (post.created_at = new Date().toLocaleDateString()),
+                (post.status = "1"),
+              ];
+              db.query(this.querySql, [this.#values], (err, result) => {
+                if (err) {
+                  if (err.message.includes("Duplicate entry")) {
+                    return reject("already exist");
+                  }
+                  reject(err.message);
+                } else {
+                  resolve(result.insertId);
                 }
-                reject(err.message);
-              } else {
-                resolve(result.affectedRows > 0 ? "updated" : result);
-              }
+              });
+            } else {
+              reject("category not found");
             }
-          );
-        }
+          }
+        });
       } else {
-        reject("invalid email or not strong password or userId");
+        reject("invalid title , categoryId  or status");
       }
     });
   }
-  getUserDetails(userId) {
+  updatePost(post) {
     return new Promise((resolve, reject) => {
-      if (typeof userId == "number" && userId > 0) {
-        this.querySql = "select * from user;";
-        db.query(this.querySql, (err, result) => {
+      if (
+        !post.title.length <= 1 &&
+        typeof post.categoryId == "number" &&
+        typeof post.id == "number" &&
+        post.status
+      ) {
+        //verificando se existe uma categoria com esse id
+        this.#querySql = "select id from categoryPost where id = ?";
+        db.query(this.#querySql, [post.categoryId], (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            if (result[0]?.id) {
+              this.querySql =
+                "update post set title = ?, description = ? , categoryId = ? , cover = ? , status = ? where id = ? limit 1;";
+              db.query(
+                this.querySql,
+                [
+                  post.title,
+                  post.description,
+                  post.categoryId,
+                  post.cover,
+                  post.status,
+                  post.id,
+                ],
+                (err, result) => {
+                  if (err) {
+                    if (err.message.includes("Duplicate entry")) {
+                      return reject("already exist");
+                    }
+                    reject(err.message);
+                  } else {
+                    resolve(result.affectedRows > 0 ? "updated" : "not found");
+                  }
+                }
+              );
+            } else {
+              reject("category not found");
+            }
+          }
+        });
+      } else {
+        reject("invalid title , categoryId  or status");
+      }
+    });
+  }
+  getPostDetails(postId) {
+    return new Promise((resolve, reject) => {
+      if (typeof postId == "number" && postId > 0) {
+        this.querySql =
+          "select post.id as postId ,post.title as postTitle , post.cover , post.description as text, post.created_at as date , post.status , categorypost.title CategoryTitle , categorypost.id CategoryId from post join categorypost on post.id = categorypost.id where post.categoryId = ? limit 1;";
+        db.query(this.querySql, [postId], (err, result) => {
           if (err) {
             reject(err.message);
           } else {
@@ -94,9 +111,9 @@ export default class User {
       }
     });
   }
-  getAllUsers() {
-    return new Promise((resolve, reject) => {
-      this.querySql = "select * from user;";
+  getAllPost() {
+    return new Promise((resolve, reject) => {this.querySql =
+      "select post.id as postId ,post.title as postTitle , post.cover , post.description as text, post.created_at as date , post.status , categorypost.title CategoryTitle , categorypost.id CategoryId from post join categorypost on post.categoryId = categorypost.id;";
       db.query(this.querySql, (err, result) => {
         if (err) {
           reject(err.message);
@@ -106,11 +123,11 @@ export default class User {
       });
     });
   }
-  deleteUser(userId) {
+  deletePost(postId) {
     return new Promise((resolve, reject) => {
-      if (typeof userId == "number" && userId > 0) {
-        this.querySql = "delete from user where id = ? limit 1;";
-        db.query(this.querySql, [userId], (err, result) => {
+      if (typeof postId == "number" && postId > 0) {
+        this.querySql = "delete from post where id = ? limit 1;";
+        db.query(this.querySql, [postId], (err, result) => {
           if (err) {
             reject(err.message);
           } else {
